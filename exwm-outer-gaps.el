283aaff4ab9e816d5a6832646d9e29c8c5a134da
PatrykGor <57003717+PatrykGor@users.noreply.github.com>
2025-02-21 10:11:35 +0100

    Fixed xcb:RECTANGLE error

--- exwm-outer-gaps.el
+++ exwm-outer-gaps.el
@@ -3,34 +3,29 @@
 (require 'exwm-core)
 (require 'exwm)
 (require 'xelb)
-
 (defgroup exwm-outer-gaps nil
   "Outer gaps for exwm."
   :group 'appearance
   :prefix "exwm-outer-gaps")
-
 (defcustom exwm-outer-gaps-width [15 15 15 15]
   "Value for the outer gaps to the left, right, top and bottom of
  the emacs frames.")
-
 (defcustom exwm-outer-gaps-increment-step 5
   "Default increment/decrement value for gaps.")
-
 (defun exwm-outer-gaps-compute-gaps ()
   "Hook to be ran after exwm-workspace--update-workareas-hook"
   (let (workareas frames)
     (dolist (w exwm-workspace--workareas)
-      (setf (aref w 0) (+ (aref w 0) (aref exwm-outer-gaps-width 0)) ; x for top left corner
-            (aref w 1) (+ (aref w 1) (aref exwm-outer-gaps-width 2)) ; y for top left corner
-            (aref w 2) (- (aref w 2) (+ (aref exwm-outer-gaps-width 0) (aref exwm-outer-gaps-width 1))) ; width of frame
-            (aref w 3) (- (aref w 3) (+ (aref exwm-outer-gaps-width 2) (aref exwm-outer-gaps-width 3))))))) ; height of frame
+      (setf (aref w 2) (+ (aref w 2) (aref exwm-outer-gaps-width 0)) ; x for top left corner
+            (aref w 3) (+ (aref w 3) (aref exwm-outer-gaps-width 2)) ; y for top left corner
+            (aref w 4) (- (aref w 4) (+ (aref exwm-outer-gaps-width 0) (aref exwm-outer-gaps-width 1))) ; width of frame
+            (aref w 5) (- (aref w 5) (+ (aref exwm-outer-gaps-width 2) (aref exwm-outer-gaps-width 3))))))) ; height of frame
 
 (defun exwm-outer-gaps-apply ()
   "Function used to apply gaps to the emacs frames."
   (exwm-workspace--update-workareas)
   (dolist (f exwm-workspace--list)
     (exwm-workspace--set-fullscreen f)))
-
 (defun exwm-outer-gaps-set (border width increment)
   "Set border in {left, right, top, bottom} to width.
 increment (bool) indicates whether the width should be used as
@@ -41,13 +36,11 @@ absolute width or as increment value."
                (aref exwm-outer-gaps-width border)
              0)
            width)))
-
 (defun exwm-outer-gaps-set-all (width increment)
   "Set all gaps. Increment (bool) determines whether the width
 should be used as absolute width or as increment value"
   (dotimes (border 4)
     (exwm-outer-gaps-set border width increment)))
-
 (defun exwm-outer-gaps-increment (arg)
   "Increment the outer gaps. If arg {0, 1, 2, 3} is passed,
 increment the corresponding {left, right, top, bottom} border.
@@ -59,7 +52,6 @@ exwm-outer-gaps-increment-step"
         (exwm-outer-gaps-set arg exwm-outer-gaps-increment-step t)
       (exwm-outer-gaps-set-all exwm-outer-gaps-increment-step t))
     (exwm-outer-gaps-apply)))
-
 (defun exwm-outer-gaps-decrement (arg)
   "Decrement the outer gaps. If arg {0, 1, 2, 3} is passed,
 decrement the corresponding {left, right, top, bottom} border.
@@ -71,7 +63,6 @@ exwm-outer-gaps-increment-step"
         (exwm-outer-gaps-set arg (- exwm-outer-gaps-increment-step) t)
       (exwm-outer-gaps-set-all (- exwm-outer-gaps-increment-step) t))
     (exwm-outer-gaps-apply)))
-
 (defun exwm-outer-gaps-balance (border)
   "Set all gaps to the width of border"
   (interactive "P")
@@ -79,7 +70,6 @@ exwm-outer-gaps-increment-step"
       (exwm-outer-gaps-set-all (aref exwm-outer-gaps-width border) nil)
     (exwm-outer-gaps-set-all (aref exwm-outer-gaps-width 0) nil))
   (exwm-outer-gaps-apply))
-
 ;;;###autoload
 (define-minor-mode exwm-outer-gaps-mode
   "Add useless outer gaps to exwm."
@@ -88,7 +78,6 @@ exwm-outer-gaps-increment-step"
       (add-hook 'exwm-workspace--update-workareas-hook
                 #'exwm-outer-gaps-compute-gaps)
     (remove-hook 'exwm-workspace--update-workareas-hook
-                 #'exwm-outer-gaps-compute-gaps))
-  (exwm-outer-gaps-apply))
+                 #'exwm-outer-gaps-compute-gaps)))
 
 (provide 'exwm-outer-gaps)
